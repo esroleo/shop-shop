c23d25478f55ff778b10bf7347299a26c16a24df
esroleo <esroleo@gmail.com>
2021-05-05 14:12:43 -0400

    stripe added to back end

---
+++ client/build/precache-manifest.49fd37c1a5b346eab0bfa73e8a49dc25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5086d873d24b855755a48efd864a49b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e3e086dc0d8457a1f3",
+    "url": "/static/css/main.9d8e7364.chunk.css"
+  },
+  {
+    "revision": "74187c13fb0227c50dde",
+    "url": "/static/js/2.e344f7ab.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e344f7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e3e086dc0d8457a1f3",
+    "url": "/static/js/main.4046c6ef.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
